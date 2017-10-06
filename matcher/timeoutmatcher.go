ec19b27ff3f751b8949125de0b0f04c8651bae94
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2017-10-06 22:04:50 +0200

    Add TimeoutMatcher with tests
    TimeoutMatcher is a Matcher interface implementation for handling
    running requests timeouts caused by expiration of requests' job timeout.
    
    It tries to run Close on all reported requests. Some of the timeouts
    might be invalid, because the request has changed it's state to done
    or failed.
    
    Tests base on using MockRequestsManager for mocking up RequestsManager
    interface.
    
    Change-Id: I6540ab50eb84f8ab1b9737ad71e7ae3a5ddd3170
    Signed-off-by: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>

---
+++ matcher/timeoutmatcher.go
@@ -0,0 +1,50 @@
+/*
+ *  Copyright (c) 2017-2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+// File matcher/timeoutmatcher.go provides TimeoutMatcher structure.
+// It implements Matcher interface and it should be used for handling timeout
+// events caused by expiration of requests' job timeout.
+
+package matcher
+
+import (
+	. "git.tizen.org/tools/boruta"
+)
+
+// TimeoutMatcher implements Matcher interface for handling running requests
+// timeout.
+type TimeoutMatcher struct {
+	Matcher
+	// requests provides internal boruta access to requests.
+	requests RequestsManager
+}
+
+// NewTimeoutMatcher creates a new TimeoutMatcher structure.
+func NewTimeoutMatcher(r RequestsManager) *TimeoutMatcher {
+	return &TimeoutMatcher{
+		requests: r,
+	}
+}
+
+// Notify implements Matcher interface. This method reacts to events passed to
+// matcher. Close method is called on RequestsManager for each request.
+// Some of the cases might be invalid, because the request's state has been changed
+// to DONE or FAILED. Verification of closing conditions is done inside Close method.
+func (m TimeoutMatcher) Notify(out []ReqID) {
+	for _, r := range out {
+		m.requests.Close(r)
+	}
+}
