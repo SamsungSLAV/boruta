cc1e4fe0fc4e6d0eba172c74e9a6cf62e1bae8eb
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2017-10-24 11:11:34 +0200

    Add WorkerChange interface
    WorkerChange interface defines API for implementation to be informed
    about changes in workers.
    
    The mock implementation of interface is provided in workers package.
    It is generated using mockgen command:
    
    mockgen	-package workers \
    	-destination=workers/workerchange_mock_test.go \
    	-write_package_comment=false \
    	git.tizen.org/tools/boruta/workers WorkerChange
    
    Change-Id: Ia17aefd747057868bf2b4b57332397a591ac038a
    Signed-off-by: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>

---
+++ workers/workerchange.go
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (c) 2017-2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+// File workers/workerchange.go defines WorkerChange interface with API
+// for notification about changes in workers' states.
+
+package workers
+
+import (
+	. "git.tizen.org/tools/boruta"
+)
+
+// WorkerChange defines API for implementation to be informed about
+// changes in workers.
+type WorkerChange interface {
+	// OnWorkerIdle notifies about available idle worker.
+	OnWorkerIdle(WorkerUUID)
+	// OnWorkerFail notifies about breaking execution of job by a running worker and
+	// putting it into FAIL or MAINTENANCE state.
+	OnWorkerFail(WorkerUUID)
+}
