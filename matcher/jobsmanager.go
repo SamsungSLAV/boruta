06115172a441a253cd8c636b3245bca61d92ec20
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2017-10-27 20:39:23 +0200

    Add JobsManager interface
    JobsManager interface defines API for taking actions on jobs.
    
    The mock implementation of interface is provided in matcher package.
    It is generated using mockgen command:
    
    mockgen	-package matcher \
    	-destination=matcher/jobsmanager_mock_test.go \
    	-write_package_comment=false \
    	git.tizen.org/tools/boruta/matcher JobsManager
    
    Change-Id: I950b68cbadd2a2e7be3d65406d36082d6ac29ee4
    Signed-off-by: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>

---
+++ matcher/jobsmanager.go
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2017-2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+// File matcher/jobsmanager.go defines JobsManager interface with API
+// for managing jobs.
+
+package matcher
+
+import (
+	. "git.tizen.org/tools/boruta"
+	"git.tizen.org/tools/boruta/workers"
+)
+
+// JobsManager defines API for internal boruta management of jobs.
+type JobsManager interface {
+	// Create prepares a new job for the worker.
+	Create(ReqID, WorkerUUID) error
+	// Get returns pointer to a Job from JobsManager or error if no job for
+	// the worker is found.
+	Get(WorkerUUID) (*workers.Job, error)
+	// Finish cleans up after job is done.
+	Finish(WorkerUUID) error
+}
---
+++ workers/job.go
@@ -0,0 +1,33 @@
+/*
+ *  Copyright (c) 2017-2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+package workers
+
+import (
+	. "git.tizen.org/tools/boruta"
+	"git.tizen.org/tools/boruta/tunnels"
+)
+
+// Job describes worker job.
+type Job struct {
+	// Access describes details of the connection to Dryad. It is returned to the request
+	// owner when a job for request is run and acquired by the user.
+	Access AccessInfo
+	// Tunnel is a connection to Dryad for the user.
+	Tunnel tunnels.Tunneler
+	// Req is ID of the worked request.
+	Req ReqID
+}
