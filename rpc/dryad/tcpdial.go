a7419534e4a2463f93b94715ddeabe39aed16891
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2018-03-05 20:08:46 +0100

    Add TCP Dial RPC client creation to DryadClient
    The Create function implements ClientManager interface.
    It sets up a new TCP dialled RPC client in DryadClient structure.
    
    The connection created this way will be used for managing Dryad
    (key generation, putting in maintenance state, preparation).
    
    Change-Id: I2d9c48d05f65e38b49aa6fe7ded10e324248f84e
    Signed-off-by: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>

---
+++ rpc/dryad/tcpdial.go
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (c) 2018 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+// File rpc/dryad/tcpdial.go contains implementation of creating an RPC
+// client inside DryadClient using TCP dial.
+
+package dryad
+
+import (
+	"net"
+	"net/rpc"
+)
+
+// Create sets up new TCP dialled RPC client in DryadClient structure.
+// The Create function implements ClientManager interface.
+func (_c *DryadClient) Create(ip net.IP, port int) error {
+	addr := &net.TCPAddr{IP: ip, Port: port}
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		return err
+	}
+	_c.client = rpc.NewClient(conn)
+	return nil
+}
